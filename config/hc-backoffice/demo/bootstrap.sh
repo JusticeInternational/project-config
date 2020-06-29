5df20e4f0570def173dd06ae0e8f2a52d1d8ef4b
Edward Raigosa <wenlock@github.com>
2020-06-29 01:53:24 +0000

    run as sudo

--- config/hc-backoffice/demo/bootstrap.sh
+++ config/hc-backoffice/demo/bootstrap.sh
@@ -7,7 +7,7 @@ set -e
 # setup docker-compose
 if [ ! -x /usr/local/bin/docker-compose ]; then
   sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 # setup ngrok https://dashboard.ngrok.com/get-started/setup
