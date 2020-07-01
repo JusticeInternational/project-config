934b25509c174c1acc19d7320ca5a6c6415574dc
Edward Raigosa <wenlock@github.com>
2020-07-01 11:39:21 -0700

    Update entrypoint.sh

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-[ -f "${HOME}/dotfiles/install.sh" ] && "${HOME}/dotfiles/install.sh"
+echo $(whoami)
+# [ -f "${HOME}/dotfiles/install.sh" ] && "${HOME}/dotfiles/install.sh"
 
 /usr/local/share/docker-init.sh
